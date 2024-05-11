2b7d42f8d727e0368b51c1940e17b0fd7e40027f
martinbolsnes <martinbolsones@gmail.com>
2024-05-12 00:38:52 +0200

    Refactor CategoryButtons to dynamically render buttons based on categories

--- app/components/CategoryButtons.tsx
+++ app/components/CategoryButtons.tsx
@@ -2,25 +2,26 @@ import { Button } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
 import { Beef, Fish, Salad, Wheat } from 'lucide-react';
 
-export const CategoryButtons = () => {
+import { getCategories } from '@/sanity/sanity.query';
+import type { CategoryType } from '@/types';
+
+const icons = {
+  meat: <Beef className={cn('w-4 h-4 mr-2')} />,
+  fish: <Fish className={cn('w-4 h-4 mr-2')} />,
+  veggie: <Salad className={cn('w-4 h-4 mr-2')} />,
+  pasta: <Wheat className={cn('w-4 h-4 mr-2')} />,
+};
+
+export const CategoryButtons = async () => {
+  const categories: CategoryType[] = await getCategories();
   return (
     <div className={cn('flex items-center gap-4')}>
-      <Button size='sm' variant='outline'>
-        <Salad className={cn('w-4 h-4 mr-2')} />
-        Veggie
-      </Button>
-      <Button size='sm' variant='outline'>
-        <Beef className={cn('w-4 h-4 mr-2')} />
-        Meat
-      </Button>
-      <Button size='sm' variant='outline'>
-        <Fish className={cn('w-4 h-4 mr-2')} />
-        Fish
-      </Button>
-      <Button size='sm' variant='outline'>
-        <Wheat className={cn('w-4 h-4 mr-2')} />
-        Pasta
-      </Button>
+      {categories.map((category) => (
+        <Button key={category._id} size='sm' variant='outline'>
+          {icons[category.icon as keyof typeof icons]}
+          {category.name}
+        </Button>
+      ))}
     </div>
   );
 };
