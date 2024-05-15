342be32a8f62a79ce10c65d0870bc59f07d97d81
martinbolsnes <martinbolsones@gmail.com>
2024-05-15 19:54:06 +0200

    Add navigation menu to the header

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,19 @@
 import { Button } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
-import { PartyPopper, Plus, Utensils } from 'lucide-react';
+import { Menu, PartyPopper, Plus, Utensils } from 'lucide-react';
 import Link from 'next/link';
+import { Label } from '@/components/ui/label';
+import { Input } from '@/components/ui/input';
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet';
 
 export const Header = () => {
   return (
@@ -13,18 +25,39 @@ export const Header = () => {
       <div>
         <Link href='/' className={cn('flex items-center gap-2')}>
           <Utensils className={cn('w-4 h-4 text-primary')} />
-          <h1 className={cn('text-lg font-bold')}>Yumz</h1>
+          <h1 className={cn('text-lg font-bold')}>MunchMate</h1>
         </Link>
       </div>
       <div className={cn('flex items-center gap-4')}>
-        <Button size='sm' variant='secondary'>
-          <Plus className={cn('w-4 h-4 mr-2')} />
-          Add Recipe
-        </Button>
         <Button size='sm' variant='default'>
           <PartyPopper className={cn('w-4 h-4 mr-2')} />
           Surprise Me
         </Button>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button variant='ghost'>
+              <Menu className={cn('w-6 h-6')} />
+            </Button>
+          </SheetTrigger>
+          <SheetContent>
+            <SheetHeader>
+              <SheetTitle>Navigation</SheetTitle>
+            </SheetHeader>
+            <div className={cn('flex flex-col items-end gap-4 py-6 text-lg')}>
+              <Link href='/recipes'>All recipes</Link>
+              <Link href='/recipes'>Categories</Link>
+              <Link href='/recipes'>About</Link>
+              <Link href='/recipes'>FAQ</Link>
+            </div>
+            <div className={cn('py-4 flex justify-end w-full')}>
+              <Button size='lg' variant='default'>
+                <Plus className={cn('w-4 h-4 mr-2')} />
+                Add Recipe
+              </Button>
+            </div>
+            <SheetFooter></SheetFooter>
+          </SheetContent>
+        </Sheet>
       </div>
     </header>
   );
