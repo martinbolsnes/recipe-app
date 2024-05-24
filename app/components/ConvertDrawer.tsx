116368d9bc48e920dffb4ce78b42cd971d3dbce2
martinbolsnes <martinbolsones@gmail.com>
2024-05-25 01:03:44 +0200

    chore: Fix typo in ConvertDrawer component class name

--- app/components/ConvertDrawer.tsx
+++ app/components/ConvertDrawer.tsx
@@ -78,7 +78,7 @@ export default function ConvertDrawer() {
               <div className={cn('flex gap-2')}>
                 <input
                   className={cn(
-                    'flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50'
+                    'taxt-base flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50'
                   )}
                   type='number'
                   value={value}
