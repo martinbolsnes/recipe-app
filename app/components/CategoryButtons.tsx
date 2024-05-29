9c9aa73569e119867e061e452cd861735369fbb6
martinbolsnes <martinbolsones@gmail.com>
2024-05-29 21:02:19 +0200

    refactor: Add "All" button to CategoryButtons component for displaying all categories

--- app/components/CategoryButtons.tsx
+++ app/components/CategoryButtons.tsx
@@ -12,12 +12,16 @@ const icons = {
   pasta: <Wheat className={cn('w-4 h-4 mr-2')} />,
   dessert: <CakeSlice className={cn('w-4 h-4 mr-2')} />,
   drinks: <Martini className={cn('w-4 h-4 mr-2')} />,
+  all: <CakeSlice className={cn('w-4 h-4 mr-2')} />,
 };
 
 export const CategoryButtons = async () => {
   const categories: CategoryType[] = await getCategories();
   return (
     <div className={cn('flex flex-wrap items-center gap-4')}>
+      <Button size='sm' variant='outline'>
+        All
+      </Button>
       {categories.map((category) => (
         <Button key={category._id} size='sm' variant='outline'>
           {icons[category.icon as keyof typeof icons]}
