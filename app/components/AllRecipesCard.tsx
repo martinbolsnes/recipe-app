bc6456d3a7e3cd5c59908a63e7013235d3c03e22
martinbolsnes <martinbolsones@gmail.com>
2024-06-01 00:47:00 +0200

    refactor: Optimize recipe image loading in AllRecipesCard component

---
+++ app/components/AllRecipesCard.tsx
@@ -0,0 +1,34 @@
+import { cn } from '@/lib/utils';
+import Link from 'next/link';
+import Image from 'next/image';
+import { RecipeType } from '@/types';
+
+export function AllRecipesCard({ recipe }: { recipe: RecipeType }) {
+  return (
+    <Link
+      href={`/recipes/${(recipe.slug as any).current}`}
+      key={recipe._id}
+      className={cn(
+        'bg-background rounded-md shadow-md transition-all hover:shadow-lg overflow-hidden'
+      )}
+    >
+      <div className={cn('relative')}>
+        <Image
+          alt={recipe.image?.alt}
+          className={cn('w-full h-48 object-cover')}
+          height={250}
+          src={recipe.image?.image}
+          style={{
+            aspectRatio: '400/250',
+            objectFit: 'cover',
+          }}
+          width={400}
+        />
+      </div>
+      <div className={cn('p-4')}>
+        <h3 className={cn('text-lg font-bold mb-2')}>{recipe.name}</h3>
+        <p className={cn('text-neutral-500 mb-4')}>{recipe.shortDescription}</p>
+      </div>
+    </Link>
+  );
+}
