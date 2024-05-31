07e8d28226ed83b54860be229f322fb5262eed19
martinbolsnes <martinbolsones@gmail.com>
2024-06-01 00:47:06 +0200

    refactor: Update CategoryButtons component to include "All" button and improve styling

--- app/components/CategoryButtons.tsx
+++ app/components/CategoryButtons.tsx
@@ -1,9 +1,12 @@
+'use client';
+
 import { Button } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
+import { useRouter, useSearchParams, usePathname } from 'next/navigation';
 import { Beef, CakeSlice, Fish, Martini, Salad, Wheat } from 'lucide-react';
-
 import { getCategories } from '@/sanity/sanity.query';
 import type { CategoryType } from '@/types';
+import { useEffect, useState } from 'react';
 
 const icons = {
   meat: <Beef className={cn('w-4 h-4 mr-2')} />,
@@ -15,23 +18,58 @@ const icons = {
   all: <CakeSlice className={cn('w-4 h-4 mr-2')} />,
 };
 
-export const CategoryButtons = async () => {
-  const categories: CategoryType[] = await getCategories();
+export default function CategoryButtons() {
+  const [categories, setCategories] = useState<CategoryType[]>([]);
+  const [selectedCategory, setSelectedCategory] = useState<string>('all');
+  const searchParams = useSearchParams();
+  const pathname = usePathname();
+  const { replace } = useRouter();
+
+  useEffect(() => {
+    async function fetchData() {
+      const fetchedCategories = await getCategories();
+      setCategories(fetchedCategories);
+    }
+
+    fetchData();
+  }, []);
+
+  const handleCategoryClick = (category: string) => {
+    setSelectedCategory(category);
+    const params = new URLSearchParams(searchParams);
+    if (category !== 'all') {
+      params.set('category', category);
+    } else {
+      params.delete('category');
+    }
+    replace(`${pathname}?${params.toString()}`);
+  };
+
   return (
     <div className={cn('flex flex-wrap items-center gap-4')}>
       <Button
         size='sm'
-        variant='outline'
-        className='bg-primary text-background'
+        variant={selectedCategory === 'all' ? 'default' : 'outline'}
+        onClick={() => handleCategoryClick('all')}
       >
+        {icons.all}
         All
       </Button>
-      {categories.map((category) => (
-        <Button key={category._id} size='sm' variant='outline'>
-          {icons[category.icon as keyof typeof icons]}
-          {category.name}
-        </Button>
-      ))}
+
+      {categories.map((category) => {
+        const categoryName = category.name.toLowerCase();
+        return (
+          <Button
+            key={category._id}
+            size='sm'
+            variant={selectedCategory === categoryName ? 'default' : 'outline'}
+            onClick={() => handleCategoryClick(categoryName)}
+          >
+            {icons[category.icon as keyof typeof icons]}
+            {category.name}
+          </Button>
+        );
+      })}
     </div>
   );
-};
+}
