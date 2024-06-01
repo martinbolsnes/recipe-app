78b500a8f8b3488c26951bdd709e42582175041d
martinbolsnes <martinbolsones@gmail.com>
2024-06-01 11:31:40 +0200

    refactor: Update middleware.ts to include clerkMiddleware and config

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,7 @@
-export { auth as middleware } from '@/app/utils/auth';
+import { clerkMiddleware } from '@clerk/nextjs/server';
+
+export default clerkMiddleware();
+
+export const config = {
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+};
