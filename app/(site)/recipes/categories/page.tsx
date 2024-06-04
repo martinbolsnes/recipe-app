0116d23a212056399c57c951db440fdf3057c602
martinbolsnes <martinbolsones@gmail.com>
2024-06-04 21:21:51 +0200

    refactor: Update recipe page to use placeholder image for missing images

--- app/(site)/recipes/categories/page.tsx
+++ app/(site)/recipes/categories/page.tsx
@@ -1,15 +1,34 @@
 import {
-  BeefIcon,
+  Beef,
   CakeSlice,
-  FishIcon,
+  Fish,
   Martini,
-  SaladIcon,
-  WheatIcon,
+  Salad,
+  Wheat,
+  ListPlus,
 } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
+import { cn } from '@/lib/utils';
+import { sanityFetch } from '@/sanity/sanity.client';
+import { CategoryType } from '@/types/index';
+import { categoriesQuery } from '@/sanity/sanity.query';
 
-export default function Component() {
+const icons = {
+  kjøtt: <Beef className={cn('w-4 h-4 mr-2')} />,
+  fisk: <Fish className={cn('w-4 h-4 mr-2')} />,
+  vegetar: <Salad className={cn('w-4 h-4 mr-2')} />,
+  pasta: <Wheat className={cn('w-4 h-4 mr-2')} />,
+  dessert: <CakeSlice className={cn('w-4 h-4 mr-2')} />,
+  drinker: <Martini className={cn('w-4 h-4 mr-2')} />,
+  alle: <ListPlus className={cn('w-4 h-4 mr-2')} />,
+};
+
+export default async function Component() {
+  const categories: CategoryType[] = await sanityFetch({
+    query: categoriesQuery,
+    tags: ['categories'],
+  });
   return (
     <main>
       <section className='w-full py-12 md:py-16 lg:py-20'>
@@ -23,124 +42,37 @@ export default function Component() {
             </p>
           </div>
           <div className='grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 lg:gap-6 xl:gap-8'>
-            <div className='relative group grid [grid-template-areas:stack] overflow-hidden rounded-lg'>
-              <Link href='#' className='absolute inset-0 z-10' prefetch={false}>
-                <span className='sr-only'>Se Vegetar Oppskrifter</span>
-              </Link>
-              <Image
-                src='/placeholder.svg'
-                alt='Breakfast'
-                width={300}
-                height={300}
-                className='[grid-area:stack] object-cover w-full aspect-square'
-              />
-              <div className='flex-1 [grid-area:stack] bg-gradient-to-t from-black/70 to-transparent group-hover:opacity-90 transition-opacity text-white p-6 justify-end flex flex-col gap-2'>
-                <div className='flex items-center gap-2'>
-                  <SaladIcon className='w-6 h-6' />
-                  <h3 className='font-semibold text-lg tracking-tight'>
-                    Vegetar
-                  </h3>
+            {categories.map((category) => (
+              <div
+                key={category._id}
+                className='relative group grid [grid-template-areas:stack] overflow-hidden rounded-lg'
+              >
+                <Link
+                  href='#'
+                  className='absolute inset-0 z-10'
+                  prefetch={false}
+                >
+                  <span className='sr-only'>
+                    Se {category.name} Oppskrifter
+                  </span>
+                </Link>
+                <Image
+                  src={category?.image?.image || '/placeholder.svg'}
+                  alt={category?.image?.alt}
+                  width={300}
+                  height={300}
+                  className='[grid-area:stack] object-cover w-full aspect-square'
+                />
+                <div className='flex-1 [grid-area:stack] bg-gradient-to-t from-black/70 to-transparent group-hover:opacity-90 transition-opacity text-white p-6 justify-end flex flex-col gap-2'>
+                  <div className='flex items-center gap-2'>
+                    {icons[category.icon as keyof typeof icons]}
+                    <h3 className='font-semibold text-lg tracking-tight'>
+                      {category.name}
+                    </h3>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className='relative group grid [grid-template-areas:stack] overflow-hidden rounded-lg'>
-              <Link href='#' className='absolute inset-0 z-10' prefetch={false}>
-                <span className='sr-only'>Se Kjøtt Oppskrifter</span>
-              </Link>
-              <Image
-                src='/placeholder.svg'
-                alt='Lunch'
-                width={300}
-                height={300}
-                className='[grid-area:stack] object-cover w-full aspect-square'
-              />
-              <div className='flex-1 [grid-area:stack] bg-gradient-to-t from-black/70 to-transparent group-hover:opacity-90 transition-opacity text-white p-6 justify-end flex flex-col gap-2'>
-                <div className='flex items-center gap-2'>
-                  <BeefIcon className='w-6 h-6' />
-                  <h3 className='font-semibold text-lg tracking-tight'>
-                    Kjøtt
-                  </h3>
-                </div>
-              </div>
-            </div>
-            <div className='relative group grid [grid-template-areas:stack] overflow-hidden rounded-lg'>
-              <Link href='#' className='absolute inset-0 z-10' prefetch={false}>
-                <span className='sr-only'>Se Fisk Recipes</span>
-              </Link>
-              <Image
-                src='/placeholder.svg'
-                alt='Dinner'
-                width={300}
-                height={300}
-                className='[grid-area:stack] object-cover w-full aspect-square'
-              />
-              <div className='flex-1 [grid-area:stack] bg-gradient-to-t from-black/70 to-transparent group-hover:opacity-90 transition-opacity text-white p-6 justify-end flex flex-col gap-2'>
-                <div className='flex items-center gap-2'>
-                  <FishIcon className='w-6 h-6' />
-                  <h3 className='font-semibold text-lg tracking-tight'>Fisk</h3>
-                </div>
-              </div>
-            </div>
-            <div className='relative group grid [grid-template-areas:stack] overflow-hidden rounded-lg'>
-              <Link href='#' className='absolute inset-0 z-10' prefetch={false}>
-                <span className='sr-only'>Se Pasta Oppskrifter</span>
-              </Link>
-              <Image
-                src='/placeholder.svg'
-                alt='Dessert'
-                width={300}
-                height={300}
-                className='[grid-area:stack] object-cover w-full aspect-square'
-              />
-              <div className='flex-1 [grid-area:stack] bg-gradient-to-t from-black/70 to-transparent group-hover:opacity-90 transition-opacity text-white p-6 justify-end flex flex-col gap-2'>
-                <div className='flex items-center gap-2'>
-                  <WheatIcon className='w-6 h-6' />
-                  <h3 className='font-semibold text-lg tracking-tight'>
-                    Pasta
-                  </h3>
-                </div>
-              </div>
-            </div>
-            <div className='relative group grid [grid-template-areas:stack] overflow-hidden rounded-lg'>
-              <Link href='#' className='absolute inset-0 z-10' prefetch={false}>
-                <span className='sr-only'>Se Dessert Oppskrifter</span>
-              </Link>
-              <Image
-                src='/placeholder.svg'
-                alt='Appetizer'
-                width={300}
-                height={300}
-                className='[grid-area:stack] object-cover w-full aspect-square'
-              />
-              <div className='flex-1 [grid-area:stack] bg-gradient-to-t from-black/70 to-transparent group-hover:opacity-90 transition-opacity text-white p-6 justify-end flex flex-col gap-2'>
-                <div className='flex items-center gap-2'>
-                  <CakeSlice className='w-6 h-6' />
-                  <h3 className='font-semibold text-lg tracking-tight'>
-                    Dessert
-                  </h3>
-                </div>
-              </div>
-            </div>
-            <div className='relative group grid [grid-template-areas:stack] overflow-hidden rounded-lg'>
-              <Link href='#' className='absolute inset-0 z-10' prefetch={false}>
-                <span className='sr-only'>Se Drinker Oppskrifter</span>
-              </Link>
-              <Image
-                src='/placeholder.svg'
-                alt='Salad'
-                width={300}
-                height={300}
-                className='[grid-area:stack] object-cover w-full aspect-square'
-              />
-              <div className='flex-1 [grid-area:stack] bg-gradient-to-t from-black/70 to-transparent group-hover:opacity-90 transition-opacity text-white p-6 justify-end flex flex-col gap-2'>
-                <div className='flex items-center gap-2'>
-                  <Martini className='w-6 h-6' />
-                  <h3 className='font-semibold text-lg tracking-tight'>
-                    Drinker
-                  </h3>
-                </div>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
       </section>
