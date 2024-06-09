3d874b59542c22596a76233a67cec8d43206124a
martinbolsnes <martinbolsones@gmail.com>
2024-06-09 19:57:00 +0200

    refactor: Update Footer component to improve navigation links

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -7,22 +7,24 @@ export const Footer = () => {
     <footer className='p-6 md:py-12 w-full bg-muted'>
       <div className='container max-w-7xl grid grid-cols-2 sm:grid-cols-3 gap-8 text-sm'>
         <div className='grid gap-1'>
-          <h3 className='font-semibold flex'>
-            <Utensils className={cn('w-4 h-4 text-primary mr-2')} />
-            MunchMate
-          </h3>
-          <Link href='#' prefetch={false}>
-            Om oss
+          <Link href='/'>
+            <h3 className='font-semibold flex'>
+              <Utensils className={cn('w-4 h-4 text-primary mr-2')} />
+              MunchMate
+            </h3>
           </Link>
+          {/* <Link href='#' prefetch={false}>
+            Om oss
+          </Link> */}
         </div>
         <div className='grid gap-1'>
-          <h3 className='font-semibold'>Oppskrifter</h3>
+          {/* <h3 className='font-semibold'>Oppskrifter</h3> */}
           <Link href='/recipes' prefetch={false}>
             Alle oppskrifter
           </Link>
-          <Link href='/recipes/categories' prefetch={false}>
+          {/* <Link href='/recipes/categories' prefetch={false}>
             Kategorier
-          </Link>
+          </Link> */}
         </div>
 
         {/* <div className='grid gap-1'>
