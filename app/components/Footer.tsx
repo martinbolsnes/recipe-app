a2746598fafa5d6df8c0d45297993d4ec2dea3e6
martinbolsnes <martinbolsones@gmail.com>
2024-06-12 21:13:02 +0200

    refactor: Update Footer component layout and links

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -6,9 +6,13 @@ import { createClient } from '../utils/supabase/server';
 export const Footer = async () => {
   const supabase = createClient();
   const { data: user } = await supabase.auth.getUser();
+
+  const getCurrentYear = () => {
+    return new Date().getFullYear();
+  };
   return (
     <footer className='p-6 md:py-12 w-full bg-muted'>
-      <div className='container max-w-7xl grid grid-cols-1 sm:grid-cols-3 gap-8 text-sm'>
+      <div className='container max-w-7xl grid grid-cols-2 sm:grid-cols-3 gap-8 text-sm'>
         <div className='grid gap-1'>
           <Link href='/'>
             <h3 className='font-semibold flex'>
@@ -16,14 +20,14 @@ export const Footer = async () => {
               MunchMate
             </h3>
           </Link>
-          {/* <Link href='#' prefetch={false}>
+          <Link href='/about' prefetch={false}>
             Om oss
-          </Link> */}
+          </Link>
         </div>
         <div className='grid gap-1'>
-          {/* <h3 className='font-semibold'>Oppskrifter</h3> */}
+          <h3 className='font-semibold'>Oppskrifter</h3>
           <Link href='/recipes' prefetch={false}>
-            Oppskrifter
+            Alle Oppskrifter
           </Link>
           {/* <Link href='/recipes/categories' prefetch={false}>
             Kategorier
@@ -31,13 +35,20 @@ export const Footer = async () => {
         </div>
         {user?.user ? (
           <div className='grid gap-1'>
+            <h3 className='font-semibold'>Konto</h3>
             <Link href='/profile'>Profil</Link>
           </div>
         ) : (
           <div className='grid gap-1'>
+            <h3 className='font-semibold'>Konto</h3>
             <Link href='/login'>Logg inn</Link>
           </div>
         )}
+      </div>
+      <div className='mt-24 text-center'>
+        <p className='text-sm'>
+          &copy; {getCurrentYear()} MunchMate. All rights reserved.
+        </p>
       </div>
     </footer>
   );
