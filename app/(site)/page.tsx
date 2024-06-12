a4e6d5bd4bb7b406d85ee04db5453928d32fb4a7
martinbolsnes <martinbolsones@gmail.com>
2024-06-12 21:12:34 +0200

    refactor: Update LandingPage component to include min-h-screen class

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -19,7 +19,7 @@ export default async function LandingPage() {
   });
 
   return (
-    <main>
+    <main className={cn('min-h-screen')}>
       <section
         className={cn(
           'mx-auto flex max-w-[980px] flex-col items-center gap-2 py-8 md:py-12 md:pb-8 lg:py-24 lg:pb-20'
