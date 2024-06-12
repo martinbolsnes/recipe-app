efb9981cfb4cecc227c36a510ee308c6c6ca154f
martinbolsnes <martinbolsones@gmail.com>
2024-06-12 13:18:11 +0200

    refactor: Update Supabase middleware to use SSR client and handle cookies

--- app/utils/supabase/middleware.ts
+++ app/utils/supabase/middleware.ts
@@ -1,29 +1,60 @@
+import { createServerClient, type CookieOptions } from '@supabase/ssr';
 import { NextResponse, type NextRequest } from 'next/server';
-import { createSupabaseReqResClient } from '../supabase/server';
 
-export async function middleware(request: NextRequest) {
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY!;
+
+export async function updateSession(request: NextRequest) {
   let response = NextResponse.next({
     request: {
       headers: request.headers,
     },
   });
 
-  const supabase = createSupabaseReqResClient(request, response);
+  const supabase = createServerClient(supabaseUrl, supabaseKey, {
+    cookies: {
+      get(name: string) {
+        return request.cookies.get(name)?.value;
+      },
+      set(name: string, value: string, options: CookieOptions) {
+        request.cookies.set({
+          name,
+          value,
+          ...options,
+        });
+        response = NextResponse.next({
+          request: {
+            headers: request.headers,
+          },
+        });
+        response.cookies.set({
+          name,
+          value,
+          ...options,
+        });
+      },
+      remove(name: string, options: CookieOptions) {
+        request.cookies.set({
+          name,
+          value: '',
+          ...options,
+        });
+        response = NextResponse.next({
+          request: {
+            headers: request.headers,
+          },
+        });
+        response.cookies.set({
+          name,
+          value: '',
+          ...options,
+        });
+      },
+    },
+  });
 
-  const {
-    data: { session },
-  } = await supabase.auth.getSession();
-
-  const user = session?.user;
-
-  // protects the "/account" route and its sub-routes
-  if (!user && request.nextUrl.pathname.startsWith('/private')) {
-    return NextResponse.redirect(new URL('/', request.url));
-  }
+  // refreshing the auth token
+  await supabase.auth.getUser();
 
   return response;
 }
-
-export const config = {
-  matcher: ['/', '/private/:path*'],
-};
