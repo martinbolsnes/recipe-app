7b9f192f7f18bac875a213c7df570a8e2f133d4c
martinbolsnes <martinbolsones@gmail.com>
2024-06-12 15:57:01 +0200

    refactor: Update Footer component layout in Footer.tsx

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -8,7 +8,7 @@ export const Footer = async () => {
   const { data: user } = await supabase.auth.getUser();
   return (
     <footer className='p-6 md:py-12 w-full bg-muted'>
-      <div className='container max-w-7xl grid grid-cols-2 sm:grid-cols-3 gap-8 text-sm'>
+      <div className='container max-w-7xl grid grid-cols-1 sm:grid-cols-3 gap-8 text-sm'>
         <div className='grid gap-1'>
           <Link href='/'>
             <h3 className='font-semibold flex'>
@@ -23,7 +23,7 @@ export const Footer = async () => {
         <div className='grid gap-1'>
           {/* <h3 className='font-semibold'>Oppskrifter</h3> */}
           <Link href='/recipes' prefetch={false}>
-            Alle oppskrifter
+            Oppskrifter
           </Link>
           {/* <Link href='/recipes/categories' prefetch={false}>
             Kategorier
