a69c1561c7bb9d0428250943b6a27d9665b47a75
martinbolsnes <martinbolsones@gmail.com>
2024-06-12 15:46:58 +0200

    refactor: Update AvatarComponent to include tooltip for user profile link

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -1,20 +1,35 @@
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { createClient } from '../utils/supabase/server';
 import Link from 'next/link';
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from '@/components/ui/tooltip';
 
 export default async function AvatarComponent() {
   const supabase = createClient();
   const { data: user, error } = await supabase.auth.getUser();
 
   return (
-    <Link href='/profile'>
-      <Avatar className='w-6 h-6'>
-        <AvatarImage
-          src={user.user?.user_metadata?.avatar_url}
-          alt='User avatar'
-        />
-        <AvatarFallback>MM</AvatarFallback>
-      </Avatar>
-    </Link>
+    <TooltipProvider>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Link href='/profile'>
+            <Avatar className='w-6 h-6'>
+              <AvatarImage
+                src={user.user?.user_metadata?.avatar_url}
+                alt='User avatar'
+              />
+              <AvatarFallback>MM</AvatarFallback>
+            </Avatar>
+          </Link>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>Profil</p>
+        </TooltipContent>
+      </Tooltip>
+    </TooltipProvider>
   );
 }
