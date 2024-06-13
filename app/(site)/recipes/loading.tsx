b6f3a9d9d6b2746cff2c525753e2fc550f752fcf
martinbolsnes <martinbolsones@gmail.com>
2024-06-13 19:18:21 +0200

    chore: Update loading component styles for recipe page

--- app/(site)/recipes/loading.tsx
+++ app/(site)/recipes/loading.tsx
@@ -5,14 +5,36 @@ export default function Loading() {
   return (
     <div className={cn('container mx-auto py-8 px-4 md:px-6')}>
       <div
-        className={cn(
-          'grid md:grid-cols-2 gap-6 lg:gap-12 items-start max-w-6xl px-4 mx-auto py-6'
-        )}
+        className={cn('grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6')}
       >
         <div className={cn('grid gap-6')}>
           <Skeleton
             className={cn(
-              'h-[250px] aspect-[800/600] rounded-xl w-full object-cover'
+              'h-[250px] w-[400px] aspect-[400/250] rounded-xl object-cover'
+            )}
+          />
+          <div className={cn('grid gap-4')}>
+            <Skeleton className={cn('h-6 w-[250px]')} />
+            <Skeleton className={cn('h-4 w-[200px]')} />
+            <Skeleton className={cn('h-4 w-[200px]')} />
+          </div>
+        </div>
+        <div className={cn('grid gap-6')}>
+          <Skeleton
+            className={cn(
+              'h-[250px] w-[400px] aspect-[400/250] rounded-xl object-cover'
+            )}
+          />
+          <div className={cn('grid gap-4')}>
+            <Skeleton className={cn('h-6 w-[250px]')} />
+            <Skeleton className={cn('h-4 w-[200px]')} />
+            <Skeleton className={cn('h-4 w-[200px]')} />
+          </div>
+        </div>
+        <div className={cn('grid gap-6')}>
+          <Skeleton
+            className={cn(
+              'h-[250px] w-[400px] aspect-[400/250] rounded-xl object-cover'
             )}
           />
           <div className={cn('grid gap-4')}>
