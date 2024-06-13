9a0a6797c23409269ebc0912ed543cac3f4c2d64
martinbolsnes <martinbolsones@gmail.com>
2024-06-13 21:59:13 +0200

    refactor: Update loading component styles for recipe page

--- app/(site)/recipes/loading.tsx
+++ app/(site)/recipes/loading.tsx
@@ -4,43 +4,29 @@ import { cn } from '@/lib/utils';
 export default function Loading() {
   return (
     <div className={cn('container mx-auto py-8 px-4 md:px-6')}>
+      <div className={cn('flex items-center justify-between mb-4')}>
+        <div className={cn('flex items-center gap-4')}>
+          <Skeleton className={cn('h-8 w-[500px]')} />
+        </div>
+      </div>
+      <div className={cn('mb-8')}>
+        <div className={cn('flex items-center justify-between mb-4')}>
+          <Skeleton className={cn('h-8 w-[500px]')} />
+        </div>
+      </div>
       <div
         className={cn('grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6')}
       >
         <div className={cn('grid gap-6')}>
           <Skeleton
             className={cn(
-              'h-[250px] w-[400px] aspect-[400/250] rounded-xl object-cover'
+              'h-[250px] w-[300px] aspect-[300/250] rounded-md object-cover'
             )}
           />
           <div className={cn('grid gap-4')}>
+            <Skeleton className={cn('h-8 w-[250px]')} />
             <Skeleton className={cn('h-6 w-[250px]')} />
-            <Skeleton className={cn('h-4 w-[200px]')} />
-            <Skeleton className={cn('h-4 w-[200px]')} />
-          </div>
-        </div>
-        <div className={cn('grid gap-6')}>
-          <Skeleton
-            className={cn(
-              'h-[250px] w-[400px] aspect-[400/250] rounded-xl object-cover'
-            )}
-          />
-          <div className={cn('grid gap-4')}>
             <Skeleton className={cn('h-6 w-[250px]')} />
-            <Skeleton className={cn('h-4 w-[200px]')} />
-            <Skeleton className={cn('h-4 w-[200px]')} />
-          </div>
-        </div>
-        <div className={cn('grid gap-6')}>
-          <Skeleton
-            className={cn(
-              'h-[250px] w-[400px] aspect-[400/250] rounded-xl object-cover'
-            )}
-          />
-          <div className={cn('grid gap-4')}>
-            <Skeleton className={cn('h-6 w-[250px]')} />
-            <Skeleton className={cn('h-4 w-[200px]')} />
-            <Skeleton className={cn('h-4 w-[200px]')} />
           </div>
         </div>
       </div>
