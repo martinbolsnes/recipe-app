60607d89cdab98894e8fbeb925721e03dc86c94e
martinbolsnes <martinbolsones@gmail.com>
2025-07-19 22:42:37 +0200

    refactor: update createClient function to await cookies for proper async handling

--- app/utils/supabase/server.ts
+++ app/utils/supabase/server.ts
@@ -4,8 +4,8 @@ import { cookies } from 'next/headers';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY!;
 
-export function createClient() {
-  const cookieStore = cookies();
+export async function createClient() {
+  const cookieStore = await cookies();
 
   // Create a server's supabase client with newly configured cookie,
   // which could be used to maintain user's session
